52136433f6ea900c08b3584076d285518aac4ce5
RCF Build <rcf@selectica.com>
2015-07-10 18:11:42 +0000

    Base Fri Jul 10 18:11:36 UTC 2015

---
+++ src/main/java/com/selectica/Base/definitions/DCPBO/conditions/ConditionManageTaskWFCreateContractScript.java
@@ -0,0 +1,7 @@
+package com.selectica.Base.definitions.DCPBO.conditions;
+
+import com.selectica.Base.eclm.definitions.shared.conditions.IsCreateContractButtonVisibleCondition;
+
+/**isCreateContractVisible*/
+public class ConditionManageTaskWFCreateContractScript extends IsCreateContractButtonVisibleCondition {
+}
---
+++ src/main/java/com/selectica/Base/definitions/DVendorBO/conditions/ConditionManageTaskWFCreateContractScript.java
@@ -0,0 +1,7 @@
+package com.selectica.Base.definitions.DVendorBO.conditions;
+
+import com.selectica.Base.eclm.definitions.shared.conditions.IsCreateContractButtonVisibleCondition;
+
+/**isCreateContractVisible*/
+public class ConditionManageTaskWFCreateContractScript extends IsCreateContractButtonVisibleCondition {
+}
